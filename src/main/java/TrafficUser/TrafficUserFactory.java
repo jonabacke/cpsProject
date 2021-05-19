a48f3169af5acd62b870cfb9004fb57648985c77
jonathan <jonathan.backes.jb@googlemail.com>
2021-05-19 08:49:38 +0200

    visualisierung prio

--- src/main/java/TrafficUser/TrafficUserFactory.java
+++ src/main/java/TrafficUser/TrafficUserFactory.java
@@ -16,8 +16,13 @@ public class TrafficUserFactory {
     public static void main(String[] args) {
         Logger.getGlobal().getParent().getHandlers()[0].setLevel(Level.INFO);
         Logger.getGlobal().getParent().getHandlers()[0].setFormatter(new LogFormatter());
-        System.out.println("Hello World");
-        new TrafficUserFactory();
+        EPriority prio;
+        if (Math.random() < 0.5) {
+            prio = EPriority.EMERGENCY;
+        } else {
+            prio = EPriority.NORMAL;
+        }
+        new TrafficUserFactory(prio);
     }
 
     private String uuid;
@@ -25,7 +30,7 @@ public class TrafficUserFactory {
     private TrafficUser trafficUser;
     private TrafficUserInvokeStub trafficUserInvokeStub;
 
-    public TrafficUserFactory() {
+    public TrafficUserFactory(EPriority prio) {
         this.uuid = UUID.randomUUID().toString();
 
         this.retain = false;
@@ -38,7 +43,7 @@ public class TrafficUserFactory {
         this.trafficUserInvokeStub = new TrafficUserInvokeStub(this.middleware);
 
         // Build Object
-        this.trafficUser = new TrafficUser(EPriority.CONSTRUCTION_VEHICLE, uuid, this.trafficUserInvokeStub);
+        this.trafficUser = new TrafficUser(prio, uuid, this.trafficUserInvokeStub);
 
         // Build CallStub
         new SkeletonStub(ITrafficUser.class.getName() + "/" + this.uuid, this.trafficUser, this.middleware, this.retain, this.qos);
